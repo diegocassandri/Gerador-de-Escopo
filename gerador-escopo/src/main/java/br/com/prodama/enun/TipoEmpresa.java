73e66ceb11668de481063e029e9accbbf65f00a9
victor.sirena <victorsirena@gmail.com>
2016-06-14 17:55:11 -0300

    Cadastro de Empresa parcial

--- gerador-escopo/src/main/java/br/com/prodama/enun/TipoEmpresa.java
+++ gerador-escopo/src/main/java/br/com/prodama/enun/TipoEmpresa.java
@@ -1,6 +1,17 @@
 package br.com.prodama.enun;
 
 public enum TipoEmpresa {
-	  FISICA,
-	  JURIDICA;
+	
+	FISICA ("Física"),
+    JURIDICA ("Jurídica");
+
+	private String descricao;
+
+	TipoEmpresa(String descricao) {
+		this.descricao = descricao;
 	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
