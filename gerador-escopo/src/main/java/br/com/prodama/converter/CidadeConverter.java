3f9354de404f63065e7d8a5bd96b6f6fc6b8678e
diegocassandri <diego_aox886@hotmail.com>
2016-06-16 17:50:03 -0300

    OK !

---
+++ gerador-escopo/src/main/java/br/com/prodama/converter/CidadeConverter.java
@@ -0,0 +1,42 @@
+package br.com.prodama.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.prodama.model.cadastro.Cidade;
+
+
+@FacesConverter(forClass = Cidade.class)
+public class CidadeConverter implements Converter {
+
+	@Inject
+	private EntityManager manager;
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		Cidade retorno = null;
+
+		if (value != null) {
+			retorno = this.manager.find(Cidade.class, Long.valueOf(value));
+		}
+
+		return retorno;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if (value != null) {
+			Long codigo = ((Cidade) value).getCodigo();
+			String retorno = (codigo == null ? null : codigo.toString());
+
+			return retorno;
+		}
+
+		return "";
+	}
+
+}
