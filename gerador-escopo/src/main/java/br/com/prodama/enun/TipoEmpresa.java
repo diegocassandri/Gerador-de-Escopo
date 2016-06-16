ef7e39923603f4619254eee68936cea6b75b7a1e
diegocassandri <diego_aox886@hotmail.com>
2016-06-16 17:33:45 -0300

    Ok. Oracle

--- gerador-escopo/src/main/java/br/com/prodama/enun/TipoEmpresa.java
+++ gerador-escopo/src/main/java/br/com/prodama/enun/TipoEmpresa.java
@@ -1,5 +1,7 @@
-<<<<<<< HEAD
+
 package br.com.prodama.enun;
+
+
 
 public enum TipoEmpresa {
 	
@@ -16,22 +18,4 @@ public enum TipoEmpresa {
 		return descricao;
 	}
 }
-=======
-package br.com.prodama.enun;
 
-public enum TipoEmpresa {
-	
-	FISICA ("Física"),
-    JURIDICA ("Jurídica");
-
-	private String descricao;
-
-	TipoEmpresa(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	}
-}
->>>>>>> branch 'master' of https://github.com/victorsirena/Gerador-de-Escopo.git
