f5d296c5dcf856aa4dc6175407512500cc86d141
victor.sirena <victorsirena@gmail.com>
2016-06-23 11:12:03 -0300

    Commit com erro

--- gerador-escopo/src/main/java/br/com/prodama/model/cadastro/FaixaColaborador.java
+++ gerador-escopo/src/main/java/br/com/prodama/model/cadastro/FaixaColaborador.java
@@ -2,10 +2,6 @@ package br.com.prodama.model.cadastro;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -13,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
